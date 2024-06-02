93ea0152c3628d79deb01312d5d8536c64e9e924
AdnanGhannam2001 <adnanghannam21@outlook.com>
2024-06-02 17:28:20 -0400

    Add Projects to Solution & Update Script

--- src/CSLox/Program.cs
+++ src/CSLox/Program.cs
@@ -1,1 +1,30 @@
-﻿
+﻿using CSLox;
+using CSLox.Core.Common;
+using CSLox.Core.Exceptions;
+
+try
+{
+    if (args.Length > 1)
+    {
+        throw new InvalidUsageException();
+    }
+
+    if (args.Length == 1)
+    {
+        Runner.RunFile(args[0]);
+    }
+    else
+    {
+        Runner.RunPrompt();
+    }
+}
+catch (LoxException exp)
+{
+    Logger.LogError(exp.Message, exp.Details);
+}
+catch (Exception exp)
+{
+    Logger.LogFatal("Unexpected exception thrown", exp.Message);
+
+    throw;
+}
---
+++ src/CSLox/Runner.cs
@@ -0,0 +1,14 @@
+namespace CSLox;
+
+public static class Runner
+{
+    public static void RunFile(string path)
+    {
+
+    }
+
+    public static void RunPrompt()
+    {
+
+    }
+}
